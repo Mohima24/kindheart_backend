9c5754c4d61a7535cd69926d9974a070e95cec79
unknown <mohimabahadur@gmail.com>
2023-05-17 01:23:03 +0530

    for cors error

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ let cors= require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://acelegalservices.vercel.app",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"],
+    credentials: true
+}));
 app.use(express.json());
 app.use("/users",userRouter);
 app.use("/products",productRouter);
